6ca3b4b39c78298a32ca600fc3789a39f80f2553
OJII3 <kawaisounayuriko@gmail.com>
2022-05-15 21:33:33 +0900

    Fix script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = () => {
     let username = prompt('Your name?') || '名無し';
     document.getElementById('name').innerHTML = username;
     
-    let rand = ~~Math.random()*5;
+    let rand = ~~(Math.random()*5);
     const array = ['凶', '吉', '小吉', '中吉', '大吉'];
     document.getElementById('result').innerHTML = array[rand];
 };
