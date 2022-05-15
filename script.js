b65fed74a0db875c42a1ee5ad5963bcd3932e172
OJII3 <kawaisounayuriko@gmail.com>
2022-05-15 14:50:50 +0900

    [Finish] Webアプリケーションを作ってみよう-2

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+    let username = prompt('Your name?') || '名無し';
+    document.getElementById('name').innerHTML = username;
+    
+    let rand = ~~Math.random()*5;
+    const array = ['凶', '吉', '小吉', '中吉', '大吉'];
+    document.getElementById('result').innerHTML = array[rand];
+};
